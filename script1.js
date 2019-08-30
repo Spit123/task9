3b22d17545f1f45fe5e01888a389adc3266c74fd
Spit123 <51290114+Spit123@users.noreply.github.com>
2019-08-30 13:47:40 +0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,11 +1,6 @@
 var country;
 var capital;
-/*
-function inputData() {
-	country = prompt("Введите страну.");
-	capital = prompt("Введите столицу");
-	addСountry(country, capital);
-}*/
+
 function inputData() {
 	do {
 		do {
@@ -40,15 +35,12 @@ function inputData() {
 	} while (!capital)
 	addСountry(country, capital);
 }
+
 function getInfo() {
 	country = prompt("Введите страну для инфо");
 	alert(getcountryInfo(country));
 }
-/*
-function countriesall() {
-	console.log(listСountries());
-}
-*/
+
 function countriesall() {
 	if (!listСountries()){
 		console.log("В хэше нет стран!");
@@ -56,6 +48,7 @@ function countriesall() {
 		console.log(listСountries());
 	}
 }
+
 function deleteData() {
 	country = prompt("Какую страну удалить?");
 	if ( country in countries) {
